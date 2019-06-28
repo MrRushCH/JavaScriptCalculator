a6773c51b2234da7a759c6f1bcef7400181511d5
Marc Rusch <mrusch@c18306-2.local>
2019-06-28 16:03:44 +0200

    Fixed Calculator

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,82 +1,94 @@
 class Calculator {
-  numbers = document.querySelectorAll(".number");
-  equals = document.querySelector(".equals");
-  display = document.querySelector(".calculation");
-  buttons = document.querySelectorAll("button");
+    numbers = document.querySelectorAll(".number");
+    equals = document.querySelector(".equals");
+    display = document.querySelector(".calculation");
+    buttons = document.querySelectorAll("button");
+    calculation = []
 
-  calculation = [];
-  equalsPressed = false;
+    equalsPressed = false;
 
-  scanButton() {
-    var buttons = this.buttons;
-    var display = this.display;
-    var pressedButton = null;
-    var calculation = [];
-    if (pressedButton !== "=") {
-      buttons.forEach(function(button) {
-        button.addEventListener("click", function() {
-          pressedButton = button.dataset.button;
-          if (pressedButton !== "=") {
-            if (button.classList.contains("number")) {
-              if (display.innerHTML === "0") {
-                display.innerHTML = pressedButton;
-              } else {
-                display.innerHTML += pressedButton;
-              }
+    scanButton() {
+        let self = this;
+        var buttons = this.buttons;
+        var display = this.display;
+        buttons.forEach(function (item) {
+            item.addEventListener('click', function () {
+                if (item.classList.contains("number")) {
+                    self.calculation.push(item.dataset.button);
+                    if (display.innerHTML === "0") {
+                        display.innerHTML = item.dataset.button;
+                    } else {
+                        display.innerHTML += item.dataset.button;
+                    }
+                } else if (item.dataset.button === "=") {
+                    self.calculate()
+                } else {
+                    self.calculation.push(item.dataset.button);
+                    display.innerHTML = "0";
+                }
+            })
+        })
+    }
+
+    calculate() {
+        console.log(this.calculation)
+        var [firstNumber, operator, secondNumber] = this.scanArray(this.calculation);
+        console.log("items:", firstNumber, operator, secondNumber);
+        var result = this.calculateResult(firstNumber, operator, secondNumber);
+        this.display.innerHTML = result;
+        console.log(result)
+    }
+
+    scanArray(calculation) {
+        console.log("scanArray started");
+        var number1 = 0;
+        var number2 = 0;
+        var operator = null;
+        console.log("CALCULATION:");
+
+        console.log(calculation)
+        calculation.forEach(function (item) {
+            console.log("item: " + item)
+            console.log(operator === null)
+            if (!(item === "+" || item === "-" || item === "*" || item === "/") && operator === null) {
+                number1 += item;
+                calculation.slice(calculation.indexOf(item));
+                console.log("Adding", item, "to", number1);
+            } else if (operator === null) {
+                operator = item;
+                console.log(operator);
             } else {
-              display.innerHTML = "0";
+                number2 += item;
+                console.log(number2);
             }
-            calculation.push(pressedButton);
-          }
         });
-      });
+        console.log("Operator: " + operator)
+        return [number1, operator, number2];
     }
-    return calculation;
-  }
-
-  scanArray() {
-    var number1;
-    var number2;
-    var operator = null;
-    this.scanButton().forEach(function(item) {
-      if (item !== "+" || item !== "-" || item !== "*" || item !== "/") {
-        number1 += item[0];
-      } else {
-        operator += item[1];
-      }
-    });
-    return [number1, operator, number2];
-  }
-  calculateResult() {
-    var result;
-    switch (this.operator) {
-      case "+":
-        result = parseFloat(this.getNumber1()) + parseFloat(this.getNumber1());
-        break;
-      case "-":
-        result = parseFloat(this.getNumber1()) - parseFloat(this.getNumber1());
-        break;
-      case "*":
-        result = parseFloat(this.getNumber1()) * parseFloat(this.getNumber1());
-        break;
-      case "/":
-        result = parseFloat(this.getNumber1()) / parseFloat(this.getNumber1());
-        break;
+    calculateResult(firstNumber, operator, secondNumber) {
+        console.log("calculation started");
+        var result = null;
+        firstNumber = parseFloat(firstNumber);
+        secondNumber = parseFloat(secondNumber);
+        console.log(firstNumber, operator, secondNumber)
+        switch (operator) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                break;
+        }
+        return result;
     }
-    return result;
-  }
-  getNumber1() {
-    return this.scanArray()[0];
-  }
-  getOperator() {
-    return this.scanArray()[1];
-  }
-  getNumber2() {
-    return this.scanArray()[2];
-  }
-  displayResult() {
-    this.display.innerHTML = this.calculateResult();
-  }
 }
 const calculator = new Calculator();
 calculator.scanButton();
