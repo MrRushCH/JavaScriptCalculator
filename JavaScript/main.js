fab40b5856bddae589bc107a4814832c5c946272
Marc Rusch <mrusch@c18306-2.local>
2019-07-30 11:27:43 +0200

    Add button animations

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,11 +1,24 @@
 class Calculator {
-  display = document.querySelector(".calculation");
-  buttons = document.querySelectorAll("button");
-  clear = document.querySelector(".AC");
+  //ctx = (operators = ["+", "-", "*", "/"]);
 
+  pressedButton = undefined;
   calculation = [];
   result = 0;
   regex = new RegExp(/[\+\-\*\/]/);
+  event = new Event("click");
+  stateClasses = {
+    active: "state-active"
+  };
+
+  constructor(ctx) {
+    //Assigns context, so that actions only affect one calculator
+    this.ctx = ctx;
+    this.display = this.ctx.querySelector(".calculation");
+    this.buttons = this.ctx.querySelectorAll("button");
+    this.clear = this.ctx.querySelector(".AC");
+    this.initializeButtons();
+    this.initializeKeyboard();
+  }
   // calculates a result from an operator, number1 and number2
   executeCalculation = {
     "+": function(number1, number2) {
@@ -22,88 +35,160 @@ class Calculator {
     }
   };
 
-  // selects an Element in an Array
-  getLastElement(element) {
-    return this.calculation.length - element;
-  }
-
   //Checks if parameter is an operator
-  isOperator = function(n) {
-    var isOperator = this.regex.test(n);
-    return isOperator;
-  };
-
-  //Checks if an array contains an operator and returns the according value
-  containsOperator(array) {
-    var containsOperator =
-      array.includes("+") ||
-      array.includes("-") ||
-      array.includes("*") ||
-      array.includes("/");
-    return containsOperator;
+  isOperator(operator) {
+    return this.regex.test(operator);
   }
 
-  //Checks if last element is an Operator
-  isElementOperator(element) {
-    var lastPressedElement = this.calculation[this.getLastElement(element)];
-    return this.isOperator(lastPressedElement);
+  // checks arrays for operators, with optional index value
+  hasOperator(element) {
+    if (element !== undefined) {
+      return this.regex.test(
+        this.calculation[this.calculation.length - element]
+      );
+    }
+    return (
+      this.calculation.includes("+") ||
+      this.calculation.includes("-") ||
+      this.calculation.includes("*") ||
+      this.calculation.includes("/")
+    );
   }
 
   // resets global Variables
   resetCalculation() {
-    this.display.innerHTML = null;
+    this.display.innerHTML = 0;
     this.calculation = [];
     this.result = 0;
   }
 
-  //scans which button is pressed and executes according actions
-  scanButton() {
+  //scans which keyboard button is pressed and executes according actions
+  initializeKeyboard() {
     var self = this;
-    self.buttons.forEach(function(item) {
-      item.addEventListener("click", function() {
-        var itemValue = item.dataset.button;
+    document.onkeydown = function(e) {
+      e = e || window.event;
+      switch (e.which || e.keyCode) {
+        case 13:
+          self.manageInput(document.querySelector(".equals")); //Enter
+          break;
+        case 48:
+          self.manageInput(document.querySelector(".zero")); //0
+          break;
+        case 49:
+          self.manageInput(document.querySelector(".one")); //1
+          break;
+        case 50:
+          self.manageInput(document.querySelector(".two")); //2
+          break;
+        case 51:
+          self.manageInput(document.querySelector(".three")); //3
+          break;
+        case 52:
+          self.manageInput(document.querySelector(".four")); //4
+          break;
+        case 53:
+          self.manageInput(document.querySelector(".five")); //5
+          break;
+        case 54:
+          self.manageInput(document.querySelector(".six")); //6
+          break;
+        case 55:
+          self.manageInput(document.querySelector(".seven")); //7
+          break;
+        case 56:
+          self.manageInput(document.querySelector(".eight")); //8
+          break;
+        case 57:
+          self.manageInput(document.querySelector(".nine")); //9
+          break;
+        case 187:
+          self.manageInput(document.querySelector(".equals")); //=
+          break;
+        case 106:
+          self.manageInput(document.querySelector(".multiply")); //*
+          break;
+        case 107:
+          self.manageInput(document.querySelector(".add")); //+
+          break;
+        case 109:
+          self.manageInput(document.querySelector(".subtract")); //-
+          break;
+        case 111:
+          self.manageInput(document.querySelector(".divide")); ///
+          break;
+        case 110:
+          self.manageInput(document.querySelector(".decimal")); //.
+          break;
+        case 27:
+          self.manageInput(document.querySelector(".AC")); //ESC
+          break;
+        case 12:
+          self.manageInput(document.querySelector(".AC")); //[x] (numpad)
+        default:
+          break;
+      }
+    };
+  }
 
-        if (item.classList.contains("number")) {
-          // auf Funktion auslagern
-          if (self.result !== 0) {
-            if (!self.isElementOperator(1, self.calculation)) {
-              self.resetCalculation();
-            } else {
-              self.display.innerHTML = 0;
-              self.result = 0;
-            }
-          }
-          self.calculation.push(itemValue);
-          //Checks when the displayed Number should be replaced and when a number should be added to it
-          self.display.innerHTML === "0" ||
-          self.isElementOperator(2, self.calculation)
-            ? (self.display.innerHTML = itemValue)
-            : (self.display.innerHTML += itemValue);
-        } else {
-          switch (true) {
-            case itemValue === "=":
-              self.handleInput();
-              break;
-            case itemValue === "AC":
-              self.resetCalculation();
-              break;
-            case self.containsOperator(self.calculation):
-              var [firstNumber, operator, secondNumber] = self.convertArray(
-                self.calculation
-              );
-              self.calculation = [];
-              self.calculation.push(
-                self.temporaryCalculation(firstNumber, operator, secondNumber),
-                itemValue
-              );
-              break;
-            default:
-              self.calculation.push(itemValue);
-              break;
-          }
-        }
+  //scans which button is pressed and executes according actions
+  initializeButtons() {
+    var self = this;
+    this.buttons.forEach(function(button) {
+      button.addEventListener("click", function() {
+        self.manageInput(button);
       });
     });
+  }
+
+  handleActiveState(button) {
+    var activeButton = this.ctx.querySelector("." + this.stateClasses.active);
+    if (activeButton) {
+      activeButton.classList.remove(this.stateClasses.active);
+    }
+    button.classList.add(this.stateClasses.active);
+  }
+
+  manageInput(button) {
+    this.handleActiveState(button);
+
+    var buttonValue = button.dataset.button;
+    if (button.classList.contains("number")) {
+      if (this.result !== 0) {
+        if (!this.hasOperator(1)) {
+          this.resetCalculation();
+        } else {
+          this.display.innerHTML = 0;
+          this.result = 0;
+        }
+      }
+      this.calculation.push(buttonValue);
+      //Checks when the displayed Number should be replaced and when a number should be added to it
+      this.display.innerHTML === "0" || this.hasOperator(2)
+        ? (this.display.innerHTML = buttonValue)
+        : (this.display.innerHTML += buttonValue);
+    } else {
+      switch (true) {
+        case buttonValue === "=":
+          this.handleInput();
+          break;
+        case buttonValue === "AC":
+          this.resetCalculation();
+          break;
+        case this.hasOperator(undefined):
+          var [firstNumber, operator, secondNumber] = this.convertArray(
+            this.calculation
+          );
+          this.calculation = [];
+          this.calculation.push(
+            this.temporaryCalculation(firstNumber, operator, secondNumber),
+            buttonValue
+          );
+          break;
+        default:
+          this.calculation.push(buttonValue);
+          break;
+      }
+    }
   }
 
   //Handles calculation and displays result on calculator display
@@ -117,15 +202,24 @@ class Calculator {
       this.calculation
     );
 
-    this.result = this.calculateResult(firstNumber, operator, secondNumber);
-
-    this.display.innerHTML = this.result;
+    this.result = this.temporaryCalculation(
+      firstNumber,
+      operator,
+      secondNumber
+    );
   }
 
   //Calculates the first 2 numbers of a calculation
   temporaryCalculation(firstNumber, operator, secondNumber) {
-    var result = this.calculateResult(firstNumber, operator, secondNumber);
-    return result;
+    if (!operator) {
+      return firstNumber;
+    }
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+    var result = this.executeCalculation[operator](firstNumber, secondNumber);
+    var roundedResult = Math.round(result * 1000) / 1000;
+    this.display.innerHTML = roundedResult;
+    return roundedResult;
   }
 
   // returns ordered Array: [number1, operator, number2]
@@ -137,31 +231,15 @@ class Calculator {
 
     array.forEach(function(item) {
       if (!self.isOperator(item) && operator === null) {
-        var setFirstNumber = true;
-      }
-      switch (true) {
-        case setFirstNumber:
-          number1 += item;
-          break;
-        case operator === null:
-          operator = item;
-          break;
-        default:
-          number2 += item;
-          break;
+        number1 += item;
+      } else if (operator === null) {
+        operator = item;
+      } else {
+        number2 += item;
       }
     });
     return [number1, operator, number2];
   }
-
-  //Calculates the result at the end of a calculation
-  calculateResult(firstNumber, operator, secondNumber) {
-    var result = null;
-    firstNumber = parseFloat(firstNumber);
-    secondNumber = parseFloat(secondNumber);
-    result = this.executeCalculation[operator](firstNumber, secondNumber);
-    return Math.round(result * 1000) / 1000;
-  }
 }
-const calculator = new Calculator();
-calculator.scanButton();
+//Creates new instance of Calculator with context as parameter
+const calculator = new Calculator(document.querySelector(".calculator1"));
